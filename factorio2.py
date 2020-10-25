6b3ccdf66582bf202225b0e52134b77c573582ae
Miner239 <siputgila@gmail.com>
2020-10-25 22:13:32 +0700

    kutip

--- factorio2.py
+++ factorio2.py
@@ -1,9 +1,9 @@
 from fractions import Fraction as F
 import json, os
 
-separator = "\n"+"="*30
-msgender = ":"*4
-outbuffer = ""
+separator = '\n'+'='*30
+msgender = ':'*4
+outbuffer = ''
 
 conn = None
 
@@ -14,7 +14,7 @@ class RecipeItem:
 def strf(inp):
     f = F(inp)
     if f.denominator>5:
-        return str(f)+" ("+str(float(round(f,5)))+")"
+        return str(f)+' ('+str(float(round(f,5)))+')'
     return str(f)
 
 class Recipe:
@@ -34,33 +34,33 @@ class Recipe:
     def __str__(self,ct=1,expand=False):
         ingstr = ''
         for i in self.ninput:
-            if expand: ingstr=ingstr + "\n"
-            ingstr=ingstr+strf(self.ninput[i]*ct)+" "+i+" + "
-        ingstr=ingstr.rstrip("+ ")
-        if expand: ingstr=ingstr + "\n"
-        ingstr=ingstr+" ==> "
+            if expand: ingstr=ingstr + '\n'
+            ingstr=ingstr+strf(self.ninput[i]*ct)+' '+i+' + '
+        ingstr=ingstr.rstrip('+ ')
+        if expand: ingstr=ingstr + '\n'
+        ingstr=ingstr+' ==> '
         for i in self.output:
-            if expand: ingstr=ingstr + "\n"
-            ingstr=ingstr+strf(self.output[i]*ct)+" "+i+" + "
-        ingstr=ingstr.rstrip("+ ")
+            if expand: ingstr=ingstr + '\n'
+            ingstr=ingstr+strf(self.output[i]*ct)+' '+i+' + '
+        ingstr=ingstr.rstrip('+ ')
         return ingstr
 
     def strme(self,ct=1,expand=False):
         return self.__str__(ct,expand)
 
 def convertRecipe(recipe):
-    ingredients = recipe["ingredients"]
-    products = recipe["products"]
+    ingredients = recipe['ingredients']
+    products = recipe['products']
     outing = {}
     outpro = {}
     for item in ingredients:
-        outing[item["name"]] = item["amount"]
+        outing[item['name']] = item['amount']
     for item in products:
-        if outpro.get(item["name"]):
-            outpro[item["name"]] += getRealProduct(item)
+        if outpro.get(item['name']):
+            outpro[item['name']] += getRealProduct(item)
         else:
-            outpro[item["name"]] = getRealProduct(item)
-    return Recipe(recipe["name"], outing, outpro)
+            outpro[item['name']] = getRealProduct(item)
+    return Recipe(recipe['name'], outing, outpro)
 
 def getRealProduct(product):    
     prod = F(0,1)
@@ -82,7 +82,7 @@ def runprogram(connection):
         outbuffer = outbuffer + strtoprint + '\n'
         if prt:
             conn.sendall(str.encode(outbuffer))
-            outbuffer = ""
+            outbuffer = ''
 
     def ninput(inpstr):
         nprint(inpstr, True)
@@ -90,12 +90,12 @@ def runprogram(connection):
     
     
     rawdat = {}
-    datpath = os.path.join("recipe-lister")
-    datfiles = ["assembling-machine","fluid","furnace","item","recipe"]
+    datpath = os.path.join('recipe-lister')
+    datfiles = ['assembling-machine','fluid','furnace','item','recipe']
 
-    nprint("Loading file...")
+    nprint('Loading file...')
     for datfile in datfiles:
-        with open(os.path.join(datpath,datfile+".json")) as openfile:
+        with open(os.path.join(datpath,datfile+'.json')) as openfile:
             rawdat[datfile] = json.load(openfile)
 
     items = []
@@ -103,10 +103,10 @@ def runprogram(connection):
     ##craftcategory = {}
 
     ##Register recipes and items
-    nprint("Registering recipes and items...")
-    for recipe in rawdat["recipe"]:
-        if ("uncompress-" not in recipe) and ("rf-" not in recipe) and ("-barrel" not in recipe):
-            convertedrecipe = convertRecipe(rawdat["recipe"][recipe])
+    nprint('Registering recipes and items...')
+    for recipe in rawdat['recipe']:
+        if ('uncompress-' not in recipe) and ('rf-' not in recipe) and ('-barrel' not in recipe):
+            convertedrecipe = convertRecipe(rawdat['recipe'][recipe])
             recipes[recipe] = convertedrecipe
             for product in convertedrecipe.output:
                 if product not in items:
@@ -123,52 +123,52 @@ def runprogram(connection):
         if init:
         ##Stage 0: Select initial item(s)
             craftitems = {}
-            selecteditem = ""
+            selecteditem = ''
             inititems = {}
             init = False
             
         while stage == 0:
-            iteminput = ninput(separator + "\nAdd initial item(s), enter 'proceed' to proceed:\n")
+            iteminput = ninput(separator + '\nAdd initial item(s), enter 'proceed' to proceed:\n')
             if iteminput in items:
-                amtinput = ninput("Item amount: ")
+                amtinput = ninput('Item amount: ')
                 try:
                     amt = F(amtinput)
                     inititems[iteminput] = amt
                 except ValueError:
-                    nprint("Invalid amount: "+amtinput)
-            elif iteminput == "proceed":
+                    nprint('Invalid amount: '+amtinput)
+            elif iteminput == 'proceed':
                 stage = 1
                 craftitems = dict(inititems)
             else:
-                nprint("Invalid item: "+iteminput)
+                nprint('Invalid item: '+iteminput)
 
         ##Stage 1: Select item to breakdown, or finish breaking down
         while stage == 1:
             currentitems = {}
             currentcounter = 1
-            selecteditem = ""
+            selecteditem = ''
             nprint(separator)
-            nprint("Select item to break down:")
+            nprint('Select item to break down:')
             for item in sorted(craftitems.keys()):
                 if craftitems[item] != 0:
-                    nprint(str(currentcounter)+". "+item+": "+strf(craftitems[item]))
+                    nprint(str(currentcounter)+'. '+item+': '+strf(craftitems[item]))
                     currentitems[currentcounter] = item
                     currentcounter = currentcounter + 1
-            selectindex = ninput("Enter index of item to break down (or enter 0 to finish): ")
+            selectindex = ninput('Enter index of item to break down (or enter 0 to finish): ')
             selectint = 0
             try:
                 selectint = int(float(selectindex))
                 selecteditem = currentitems[selectint]
                 stage = 2
             except ValueError:
-                nprint("Invalid index: "+selectindex)
+                nprint('Invalid index: '+selectindex)
                 continue
             except KeyError:
                 if selectint == 0:
                     stage = 3
                     break
                 else:
-                    nprint("Index out of bounds.")
+                    nprint('Index out of bounds.')
                 
 
         ##Stage 2: Select recipe for breaking down item
@@ -179,7 +179,7 @@ def runprogram(connection):
                     mult = craftitems[selecteditem]/recipes[recipe].output[selecteditem]
                     availablerecipes[recipe] = mult
             if len(availablerecipes) == 0:
-                nprint("No recipe available for use with this item.")
+                nprint('No recipe available for use with this item.')
                 stage = 1
                 break
             else:
@@ -189,26 +189,26 @@ def runprogram(connection):
                     currentrecipes = {}
                     currentcounter = 1
                     nprint(separator)
-                    nprint("Select recipe to use:")
+                    nprint('Select recipe to use:')
                     for recipe in availablerecipes:
-                        nprint("\n"+str(currentcounter) + ". " + str(recipe) + " ["+strf(availablerecipes[recipe])+"]")
-                        nprint("[ "+recipes[recipe].strme(availablerecipes[recipe])+" ]") ##multiply recipe display with mult
+                        nprint('\n'+str(currentcounter) + '. ' + str(recipe) + ' ['+strf(availablerecipes[recipe])+']')
+                        nprint('[ '+recipes[recipe].strme(availablerecipes[recipe])+' ]') ##multiply recipe display with mult
                         currentrecipes[currentcounter] = recipe
                         currentcounter = currentcounter + 1
-                    selectindex = ninput("\nEnter index of recipe to use (or enter 0 to cancel): ")
+                    selectindex = ninput('\nEnter index of recipe to use (or enter 0 to cancel): ')
                     selectint = 0
                     try:
                         selectint = int(float(selectindex))
                         selectedrecipe = currentrecipes[selectint]
                     except ValueError:
-                        nprint("Invalid index.")
+                        nprint('Invalid index.')
                         continue
                     except KeyError:
                         if selectint == 0:
                             stage = 1
                             break
                         else:
-                            nprint("Index out of bounds.")
+                            nprint('Index out of bounds.')
                             continue
 
                 recipeused = recipes[selectedrecipe]
@@ -219,8 +219,8 @@ def runprogram(connection):
                 for item in recipeused.output:
                     craftitems[item] = (craftitems.get(item,0)
                                        -recipeused.output[item]*m)
-                nprint("\nRecipe "+str(selectedrecipe)+" crafted "+str(m)+"x :")
-                nprint("["+recipeused.strme(m)+"]")
+                nprint('\nRecipe '+str(selectedrecipe)+' crafted '+str(m)+'x :')
+                nprint('['+recipeused.strme(m)+']')
 
                 stage = 1
                     
@@ -240,16 +240,16 @@ def runprogram(connection):
                 else:
                     totaloutput[item] = -itemamt
 
-            totalrecipe = Recipe("Total",totalinput,totaloutput)
+            totalrecipe = Recipe('Total',totalinput,totaloutput)
             nprint(separator)
-            nprint("Complete recipe:")
+            nprint('Complete recipe:')
             nprint(totalrecipe.strme(expand=True))
             prompttocheck = True
             while prompttocheck:
-                checkmore = ninput("\nCheck new recipes? (y/n)").lower()
-                if checkmore in "yn":
+                checkmore = ninput('\nCheck new recipes? (y/n)').lower()
+                if checkmore in 'yn':
                     prompttocheck = False
-                    if checkmore == "y":
+                    if checkmore == 'y':
                         stage = 0
                         init = True
                         break
